775fbd4f61e1b732d7590ac059968c48554ca35f
Alejandro Palmar <52765355+alejoseed@users.noreply.github.com>
2023-02-16 08:58:21 -0500

    Scrape the ID of the mangas of MangaDex

---
+++ getID.py
@@ -0,0 +1,48 @@
+import requests 
+import pprint
+import csv
+import time
+baseurl = "https://api.mangadex.org/manga/random/"
+params = {
+    'includes[]': [
+        'manga',
+        'cover_art',
+    ],
+    'contentRating[]': [
+        'safe',
+        'suggestive',
+        'erotica',
+        'pornographic',
+    ],
+    'includedTagsMode': 'AND',
+    'excludedTagsMode': 'OR',
+}
+
+def main():
+        
+    with open('mangaIDs.csv') as csvfile:
+        data = csvfile.readlines() 
+        lastRow = data[-1]     
+        while True:
+            r = requests.get(baseurl, params=params)
+            ## Add a time delay here for 2 seconds
+            time.sleep(2)
+
+            data = r.json()
+            title = (data['data']['attributes']['title']['en'])
+            mangaid = (data['data']['id'])
+            contentrating = (data['data']['attributes']['contentRating'])
+
+            if mangaid not in lastRow:
+                with open('mangaIDs.csv', 'a') as csvfile:
+                    fieldnames = ['Title', 'MangaID', 'ContentRating']
+                    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                    writer.writerow({'Title': title, 'MangaID': mangaid, 'ContentRating': contentrating})
+                    print("Title: " + title + " MangaID: " + mangaid + " ContentRating: " + contentrating)
+            
+            csvfile.close()
+            
+
+
+if __name__ == '__main__':
+    main()
